c21a8107d0fb1b4f988a4f59c7c9b6b719528120
Yuanqing Wang <wangyq+gh@wangyq.net>
2024-03-04 00:30:35 -0500

    edge model

--- bronx/models/structural/edge.py
+++ bronx/models/structural/edge.py
@@ -1,4 +1,6 @@
 import torch
+import dgl
+from dgl import DGLGraph
 
 class EdgeModel(torch.nn.Module):
     def __init__(
@@ -7,6 +9,21 @@ class EdgeModel(torch.nn.Module):
             out_features: int,
     ):
         super().__init__()
-        self.fc = torch.nn.Linear(in_features, out_features)
+        self.fc = torch.nn.Linear(2*in_features, out_features)
+
+    def forward(
+            self,
+            g: DGLGraph,
+            h: torch.Tensor,
+    ):
+        g = g.local_var()
+        g.ndata["h"] = h
+        g.apply_edges(
+            func=lambda edges: {
+                "h": torch.cat([edges.src["h"], edges.dst["h"]], dim=-1)
+            },
+        )
+        e = self.fc(g.edata["h"])
+        return e
 
     
