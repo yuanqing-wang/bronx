2b1d450ed9170b8e794315845da0facf838e0abf
Yuanqing Wang <wangyq@wangyq.net>
2024-04-03 17:48:10 -0400

    lightning model init

--- bronx/models/model.py
+++ bronx/models/model.py
@@ -2,6 +2,7 @@ import abc
 import torch
 from torch.utils.data import Dataset
 from torch.distributions import Distribution
+import lightning as pl
 
 class BronxModel(abc.ABC):
     """Base class for all models in the Bronx framework.
@@ -21,5 +22,20 @@ class BronxModel(abc.ABC):
     @abc.abstractmethod
     def train(self, dataset: Dataset) -> None:
         pass
+
+class BronxLightningWrapper(pl.LightningModule):
+    def __init__(self, model: BronxModel):
+        super().__init__()
+        self.model = model
+
+    def forward(self, *args, **kwargs):
+        return self.model(*args, **kwargs)
+
+    def training_step(self, batch, batch_idx):
+        loss = self.model(*batch)
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.model.parameters()
 
 
