a137485889265f686968c9dc68fb891e23da6b41
Yuanqing Wang <wangyq@wangyq.net>
2024-04-15 16:46:53 -0400

    GP unwrapped

---
+++ bronx/models/strategy/functional/layer.py
@@ -0,0 +1,108 @@
+import torch
+import gpytorch
+import dgl
+
+class GPLayer(gpytorch.models.ApproximateGP):
+    def __init__(
+            self,
+            num_dim,
+            grid_bounds=(-1, 1),
+            grid_size=64,
+    ):
+        variational_distribution = gpytorch.variational\
+        .CholeskyVariationalDistribution(
+            num_inducing_points=grid_size,
+            batch_shape=torch.Size([num_dim]),
+        )
+
+        variational_strategy = gpytorch.variational.IndependentMultitaskVariationalStrategy(
+            gpytorch.variational.GridInterpolationVariationalStrategy(
+                self,
+                grid_size=grid_size,
+                grid_bounds=[grid_bounds],
+                variational_distribution=variational_distribution,
+            ),
+            num_tasks=num_dim,
+        )
+
+        super().__init__(variational_strategy)
+
+        self.covar_module = gpytorch.kernels.ScaleKernel(
+                gpytorch.kernels.RBFKernel(
+            ),
+        )
+
+        self.mean_module = gpytorch.means.ConstantMean()
+        self.grid_bounds = grid_bounds
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+    
+
+class GraphGPLayer(gpytorch.Module):
+    """ A layer that applys GP on graph data.
+
+    Parameters
+    ----------
+    layer : torch.nn.Module
+        The layer that is applied before GP.
+
+    in_features : int
+        The number of input features.
+
+    out_features : int
+        The number of output features.
+
+    grid_bounds : tuple of float, optional
+        The bounds of the grid.
+
+    grid_size : int, optional
+        The size of the grid.
+
+    Examples
+    --------
+    >>> import torch
+    >>> from bronx.models.zoo.dgl import GCN
+    >>> layer = GraphGPLayer(
+    ...     layer=GCN,
+    ...     in_features=10,
+    ...     out_features=20,
+    ... )
+    >>> g = dgl.rand_graph(5, 20)
+    >>> h = torch.rand(5, 10)
+    >>> h = layer(g, h)
+    >>> h.shape()
+    torch.Size([5, 20])
+    """
+    def __init__(
+            self,
+            layer,
+            in_features,
+            out_features,
+            grid_bounds=(-1, 1),
+            grid_size=64,
+    ):
+        super().__init__()
+        self.layer = layer(
+            in_features, out_features,
+        )
+
+        self.gp_layer = GPLayer(
+            num_dim=out_features,
+            grid_bounds=grid_bounds,
+            grid_size=grid_size,
+        )
+
+    def forward(
+            self,
+            g: dgl.DGLGraph,
+            h: torch.Tensor,
+    ):
+        h = self.layer(g, h).tanh()
+        h = h.transpose(-1, -2).unsqueeze(-1)
+        h = self.gp_layer(h)
+        return h
+
+
--- bronx/models/strategy/functional/model.py
+++ bronx/models/strategy/functional/model.py
@@ -1,4 +1,31 @@
 import torch
 import dgl
 import gpytorch
+from .layer import GraphGPLayer
 
+class UnwrappedFunctionalModel(gpytorch.Module):
+    def __init__(
+            self,
+            layer: torch.nn.Module,
+            in_features: int,
+            out_features: int,
+            hidden_features: int,
+            depth: int,
+            activation: torch.nn.Module = torch.nn.SiLU(),
+            proj_in: bool = False,
+            proj_out: bool = False,
+    ):
+        super().__init__()
+        if proj_in:
+            self.proj_in = torch.nn.Linear(in_features, hidden_features)
+        if proj_out:
+            self.proj_out = torch.nn.Linear(hidden_features, out_features)
+        
+        self.layers = torch.nn.ModuleList([
+            GraphGPLayer(
+                layer=layer,
+                in_features=in_features if i == 0 else hidden_features,
+                out_features=hidden_features if i < depth - 1 else out_features,
+            )
+            for i in range(depth)
+        ])
