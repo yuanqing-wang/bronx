038656ccb3f223aa75a2671ee276b72a5c35a3bb
yuanqing-wang <wangyq@wangyq.net>
2024-05-18 19:05:50 -0400

    jobid

--- bronx/active/experiment.py
+++ bronx/active/experiment.py
@@ -2,6 +2,7 @@ import torch
 import dgl
 import random
 import lightning as pl
+import os
 
 def experiment(
         model,
@@ -20,7 +21,12 @@ def experiment(
             dataset=portfolio,
             batch_size=len(portfolio),
         )
-        trainer = pl.Trainer(max_epochs=num_epochs)
+        trainer = pl.Trainer(
+            max_epochs=num_epochs, logger=False,
+            default_root_dir=os.path.join(
+                os.getcwd(), str(os.environ.get("SLURM_JOB_ID")),
+            )
+        )
         trainer.fit(model, data_train)
         _, g, y = next(iter(data))
         scores = acquisition(model, g, g.ndata["h"], best=y_best)
